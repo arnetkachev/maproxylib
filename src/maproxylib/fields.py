25e4b4a229a125b30bc7f99960593cf90c62378e
Artem Netkachev <arnetkachev@yandex.ru>
2025-05-28 13:48:19 +0300

    Split utils functions to utils.py

--- src/maproxylib/fields.py
+++ src/maproxylib/fields.py
@@ -26,46 +26,10 @@ Example:
 
 from typing import Any, Callable, Generic, Optional, TypeVar, cast
 
-from maproxylib.storages import BaseStorage
-
-from .storages import Storage  # Assuming this is your own storage implementation
+from .storages import BaseStorage, Storage
+from .utils import _normalize_path, _simple_return_value
 
 T = TypeVar("T", type, Any)
-
-
-def _simple_return_value(value: Any) -> Any:  # noqa: ANN401
-    """Default value converter that simply returns the input."""
-    return value
-
-
-def _normalize_path(
-    key: str | None = None,
-    path: str | list[str] | None = None,
-) -> tuple[str | None, list[str]]:
-    """
-    Converts dot-separated strings into path lists and merges with provided path.
-
-    Example:
-        key="a.b.c", path=["x", "y"] => ("c", ["x", "y", "a", "b"])
-
-    This function helps construct paths through nested dictionaries or object attributes.
-    """
-    key_path: list[str] = []
-    match key:
-        case None:
-            key_path, key = [], None
-        case str():
-            *key_path, key = key.split(".")
-    match path:
-        case None:
-            path = key_path
-        case str():
-            path = path.split(".") + key_path
-        case list():
-            path += key_path
-        case _:
-            raise TypeError(f"Unknown path type: {path=}")
-    return key, path
 
 
 class ProxyField(Generic[T]):
